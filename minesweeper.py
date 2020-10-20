de0e6eeeed5e6f8a92d182abbb25a05008043da1
PapiBert <73197571+PapiBert@users.noreply.github.com>
2020-10-20 22:46:06 +0200

    Add files via upload

--- minesweeper.py
+++ minesweeper.py
@@ -3,10 +3,8 @@ import sys
 import numpy as np
 from scipy import signal
 import random
-"""Aardige poging om minesweeper na te maken. De volgende dingen missen nu:
-
-    - Voorkomen dat eerste klik een mijn is."""
-
+"""This is a minesweeper game coded as practice with pygame and OOP in general. Everything is
+made by myself, except for the SpriteSheet class."""
 
 class SpriteSheet:
     """Class to turn a sprite sheet into several sprites. Ripped from the internet."""
@@ -62,21 +60,24 @@ class Game:
         # Set bombs and make sure that the clicked cell is no bomb
         self.bombs = random.sample(range(self.n_cells-1), self.n_bombs)
         self.bombs = [bomb + 1 if bomb >= cell_no else bomb for bomb in self.bombs]
-        self.surround_grid = self.count_bombs()
+        self.surround_grid = self.count_surrounding_bombs()
         for i, c in enumerate(self.cells):
             if i in self.bombs:
                 c.is_bomb = True
             else:
-                c.value = self.surround_grid[i]
+                c.surrounding = self.surround_grid[i]
 
-    def count_bombs(self):
+    def count_surrounding_bombs(self):
+        """Counts bombs and return a numpy array with the number of bombs surrounding each cell.
+        The value will be 0 on the bomb locations"""
         bomb_grid = np.zeros((self.n_cells_x, self.n_cells_y))
         for bomb in self.bombs:
-            bomb_grid[bomb // self.n_cells_y, bomb % self.n_cells_y] = 1
+            bomb_grid[bomb // self.n_cells_y, bomb % self.n_cells_y] = 1    # generate 2d bomb grid
         kernel = np.ones((3, 3), dtype=int)
         kernel[1, 1] = 0
         surround_grid = signal.convolve2d(bomb_grid, kernel, mode="same")
-        bomb_x, bomb_y = np.where(bomb_grid == 1)
+
+        bomb_x, bomb_y = np.where(bomb_grid == 1)           # return 0 at bomb locations
         for i in range(int(sum(sum(bomb_grid)))):
             surround_grid[bomb_x[i], bomb_y[i]] = 0
         return surround_grid.flatten()
@@ -85,12 +86,21 @@ class Game:
         """Calls the click_update() method if the cell if this cell was not clicked. Calls the update method for
         the surrounding cells recursively if the cell value is 0"""
         cell_no = self.n_cells_y * clicked_cell.x + clicked_cell.y
-        if self.n_clicked_cells == 0:
+
+        if self.n_clicked_cells == 0:                           # generates bombs if no cell is clicked yet.
             self.set_bombs(cell_no)
+
         if clicked_cell.is_clicked:
             return
-        self.n_clicked_cells += clicked_cell.click_update()
-        if clicked_cell.value == 0:
+
+        if clicked_cell.is_bomb:
+            self.lost = True                                    # game is lost when bomb is clicked
+            for bomb_cell in self.cells:                        # clicks all cells with bombs to show solution
+                if bomb_cell.is_bomb:
+                    bomb_cell.click_update()
+
+        self.n_clicked_cells += clicked_cell.click_update()     # 'clicks' all surrounding cells if value is 0
+        if clicked_cell.surrounding == 0:
             if clicked_cell.x != 0:
                 self.click_update_cell(self.cells[cell_no - self.n_cells_y])
                 if clicked_cell.y != 0:
@@ -113,39 +123,35 @@ class Game:
 
 
 class Cell(pygame.sprite.Sprite):
-    """Create a cell object that has no bomb or value. These are set by the game object.
+    """Create a cell object that has no bomb or value. These are set by the game instance.
         provide the x and y values of the grid, not of the pixels in the window."""
     def __init__(self, x_cell, y_cell):
         super().__init__()
         self.x = x_cell
         self.y = y_cell
 
-        """Set """
+        #Set unclicked sprites at right position
         self.surf = pygame.Surface((TILE_SIZE, TILE_SIZE))
         self.image = sprites_dict.get('long')
         self.pos = vec(TILE_SIZE * x_cell, TILE_SIZE * y_cell)
         self.rect = self.surf.get_rect(topleft=(int(self.pos.x), int(self.pos.y)))
-        self.is_clicked = False
-        self.is_bomb = False
-        self.value = None
-        self.is_flagged = False
+        self.is_clicked = False     # will be True after clicking
+        self.is_bomb = False        # will be True if set as bomb
+        self.surrounding = None     # will contain number of surrounding bombs
+        self.is_flagged = False     # will be True if right-clicked
 
     def click_update(self):
-        if self.is_clicked:
-            return 0
+        """Updates sprites if this cell is clicked and was not clicked before."""
         self.is_clicked = True
         if self.is_bomb:
             self.image = sprites_dict.get('bomb')
             return 0
-        elif self.value == 0:
-            self.image = sprites_dict.get('0')
-            return 1
         else:
-            self.image = sprites_dict.get(str(int(self.value)))
-            return 1
+            self.image = sprites_dict.get(str(int(self.surrounding)))
+            return 1                # change sprite to corresponding one with value
 
     def flag_update(self):
-        """Update status whether a point is flagged or not and change cell sprite"""
+        """Update flag sprite whether a point is flagged or not and change cell sprite."""
         if self.is_clicked:
             return
         elif self.is_flagged:
@@ -167,72 +173,67 @@ def load_sprites(tilesize):
     sprites_dict_out = dict(zip(sprite_names, sprite_list_resized))
     return sprites_dict_out
 
+if __name__ == '__main__':
+    # Initialize pygame
+    pygame.init()
+    vec = pygame.math.Vector2
 
-# Initialize pygame
-pygame.init()
-vec = pygame.math.Vector2
+    # Set game and tile parameters
+    N_X = 9
+    N_Y = 9
+    N_BOMBS = 10
+    N_CELLS = N_Y * N_X
+    TILE_SIZE = 48
+    FPS = 60
+    SIZE = (N_X * TILE_SIZE, N_Y * TILE_SIZE)
 
-# Set game and tile parameters
-N_X = 3
-N_Y = 3
-N_CELLS = N_Y * N_X
-N_BOMBS = N_CELLS - 1
-TILE_SIZE = 48
-FPS = 30
-SIZE = (N_X * TILE_SIZE, N_Y * TILE_SIZE)
+    # Fix display parameters
+    FramePerSec = pygame.time.Clock()
+    screen = pygame.display.set_mode(SIZE)
+    pygame.display.set_caption("Minesweeper v1")
 
-# Fix display parameters
-FramePerSec = pygame.time.Clock()
-screen = pygame.display.set_mode(SIZE)
-pygame.display.set_caption("Minesweeper sprite test")
+    # Load images and start game
+    sprites_dict = load_sprites(TILE_SIZE)
+    game = Game(N_X, N_Y, N_BOMBS)
+    all_sprites = pygame.sprite.Group()
+    for cell in game.cells:
+        all_sprites.add(cell)
 
-# Load images and start game
-sprites_dict = load_sprites(TILE_SIZE)
-game = Game(N_X, N_Y, N_BOMBS)
-all_sprites = pygame.sprite.Group()
-for cell in game.cells:
-    all_sprites.add(cell)
-
-# Initialize game variables
-
-
-while not(game.won or game.lost):
-    if game.n_clicked_cells == N_CELLS - N_BOMBS:        # check if game is won
-        game.won = True
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:               # quit if x on the game gui is clicked
-            pygame.quit()                           # close the game window
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:        # quit if "esc" key pressed
-                pygame.quit()                       # close the game window
-        if event.type == pygame.MOUSEBUTTONDOWN:    # check clicked cells
-            x, y = event.pos                        # obtain clicked pixel
-            for cell in game.cells:                 # check collision for every cell
-                if cell.rect.collidepoint(x, y):
-                    if event.button == 1:           # click_update if left click
-                        game.click_update_cell(cell)
-                        if cell.is_bomb:
-                            game.lost = True
-                    if event.button == 3:           # flag update if right click
-                        cell.flag_update()
-
-    all_sprites.draw(screen)
-    pygame.display.update()
-    FramePerSec.tick(60)
-
-
-if game.won:
-    print('You won the game you dreamgozer')
-    while True:
+    while not(game.won or game.lost):
+        if game.n_clicked_cells == N_CELLS - N_BOMBS:        # check if game is won
+            game.won = True
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+            if event.type == pygame.QUIT:               # quit if x on the game gui is clicked
+                pygame.quit()                           # close the game window
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:        # quit if "esc" key pressed
+                    pygame.quit()                       # close the game window
+            if event.type == pygame.MOUSEBUTTONDOWN:    # check clicked cells
+                x, y = event.pos                        # obtain clicked pixel
+                for cell in game.cells:                 # check collision for every cell
+                    if cell.rect.collidepoint(x, y):
+                        if event.button == 1:           # click_update if left click
+                            game.click_update_cell(cell)
+                        if event.button == 3:           # flag_update if right click
+                            cell.flag_update()
 
-elif game.lost:
-    print('You lost you droeftoeter')
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+        all_sprites.draw(screen)
+        pygame.display.update()
+        FramePerSec.tick(FPS)
+
+
+    if game.won:
+        print('You won the game you dream player')
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+    elif game.lost:
+        print('You lost you mediocre player')
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
